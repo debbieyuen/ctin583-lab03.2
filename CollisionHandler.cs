ae1c57e21052ac357fabdfa4c62da312364a8d31
Debbie Yuen <deborahyuen2016@berkeley.edu>
2024-01-23 23:51:42 -0800

    3.2 lab files

---
+++ CollisionHandler.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private ParticleSystem gemParticles;
+
+    
+
+    // Set the materials in the inspector
+    public Material[] myMaterials;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        switch (collision.gameObject.tag)
+        {
+            case "Gem":
+                Debug.Log("You won a gem!");
+                Destroy(collision.gameObject);
+                WinGem();
+                break;
+            case "Enemy":
+                Debug.Log("You hit an opponent");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Start()
+    {
+        //gameObject.GetComponent<Renderer>().material =
+        //myMaterials[Random.Range(0, myMaterials.Length)];
+        StartCoroutine(loopDelay());
+    }
+
+    IEnumerator loopDelay()
+    {
+        for (int i = 0; i < myMaterials.Length; i++)
+        {
+            meshRenderer.material = myMaterials[i];
+            yield return new WaitForSeconds(2.00f);
+        }
+        StartCoroutine(loopDelay());
+    }
+
+    private void WinGem()
+    {
+        gemParticles.Play();
+    }
+}
+
